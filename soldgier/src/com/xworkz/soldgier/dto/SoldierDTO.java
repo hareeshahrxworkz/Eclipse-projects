050df761f5d2ecc0c9a43e3cd7520f7f7ecb5e31
hareeshahr.xworkz@gmail.com <hareeshahr.xworkz@gmail.com>
2023-01-18 21:32:07 +0530

    dto

--- soldgier/src/com/xworkz/soldgier/dto/SoldierDTO.java
+++ soldgier/src/com/xworkz/soldgier/dto/SoldierDTO.java
@@ -1,5 +1,7 @@
 package com.xworkz.soldgier.dto;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
@@ -20,21 +22,23 @@ import lombok.ToString;
 public class SoldierDTO extends AbstractAuditDTO {
 	@NotNull
 	@NotBlank
-	@Size(min = 3, max = 20, message = "name min  3 and max 20 ")
+	@Size(min = 3, max = 20, message = "name must be min  3 and max 20 ")
 	private String name;
+	@Min(value = 10, message = "age must be grether than 10 less than 150")
+	@Max(150)
 	private int age;
 	@NotNull
 	@NotBlank
-	@Size(min = 5, max = 30, message = "country min  5 and max 30 ")
+	@Size(min = 5, max = 30, message = "country must be min  5 and max 30 ")
 	private String country;
 	@NotNull
 	@NotBlank
-	@Size(min = 2, max = 10, message = "rank min  2 and max 10 ")
+	@Size(min = 2, max = 10, message = "rank must be min  2 and max 10 ")
 	private String rank;
 	@NotNull
 	@NotBlank
-	@Size(min = 3, max = 150, message = "datallain min  3 and max 150 ")
-	private String datallain;
+	@Size(min = 3, max = 150, message = "datallain must be min  3 and max 150 ")
+	private String batallain;
 
 	private boolean alive;
 
